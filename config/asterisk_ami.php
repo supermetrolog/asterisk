d329f5b85e30f4c5c57b77ad3031490ede3a5599
Абдурашидов Тимур <tim-a@realtor.ru>
2021-12-24 17:53:25 +0300

    change gitignore

---
+++ config/asterisk_ami.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'host' => '10.1.3.110',
+    'scheme' => 'tcp://',
+    'port' => 5038,
+    'username' => 'test',
+    'secret' => 'fuck',
+    'connect_timeout' => 10,
+    'read_timeout' => 300000 // 5 min
+];
